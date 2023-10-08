38cf91fd313c8c1f576437ef1a4795ddb76dbd1e
sangmmagariaravind123 <sangmmagariaravind123@gmail.com>
2023-10-08 16:54:57 +0530

    .

--- player_buttons.js
+++ player_buttons.js
@@ -26,6 +26,13 @@ songItems.forEach((element, i)=>{
     element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
     element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
 })
+fileInput.addEventListener('change', () => {
+            const file = fileInput.files[0];
+            if (file) {
+                addSongToQueue(file);
+                fileInput.value = ''; // Clear the file input
+            }
+        });
  
 
 // Handle play/pause click
@@ -107,3 +114,122 @@ document.getElementById('previous').addEventListener('click', ()=>{
     masterPlay.classList.remove('fa-play-circle');
     masterPlay.classList.add('fa-pause-circle');
 })
+const audio = document.getElementById('audio');
+const playButton = document.getElementById('playButton');
+const pauseButton = document.getElementById('pauseButton');
+const nextButton = document.getElementById('nextButton');
+const clearQueueButton = document.getElementById('clearQueueButton');
+const fileInput = document.getElementById('fileInput');
+const addSongButton = document.getElementById('addSongButton');
+const queue = [];
+let currentSongIndex = -1;
+
+playButton.addEventListener('click', () => {
+    if (currentSongIndex >= 0 && currentSongIndex < queue.length) {
+        audio.play();
+    }
+});
+
+pauseButton.addEventListener('click', () => {
+    audio.pause();
+});
+
+nextButton.addEventListener('click', () => {
+    playNextSong();
+});
+
+clearQueueButton.addEventListener('click', () => {
+    clearQueue();
+});
+
+fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    if (file) {
+        addSongToQueue(file);
+        fileInput.value = ''; // Clear the file input
+    }
+});
+
+addSongButton.addEventListener('click', () => {
+    fileInput.click(); // Trigger file input click event
+});
+
+audio.addEventListener('ended', () => {
+    playNextSong();
+});
+
+function addSongToQueue(file) {
+    queue.push(file);
+    displayQueue();
+    if (currentSongIndex === -1) {
+        playNextSong();
+    }
+}
+
+function playNextSong() {
+    if (currentSongIndex < queue.length - 1) {
+        currentSongIndex++;
+        audio.src = URL.createObjectURL(queue[currentSongIndex]);
+        audio.play();
+        displayQueue();
+    } else {
+        // End of the queue, stop playback
+        audio.src = '';
+        currentSongIndex = -1;
+        displayQueue();
+    }
+}
+
+function clearQueue() {
+    queue.length = 0;
+    audio.src = '';
+    currentSongIndex = -1;
+    displayQueue();
+}
+
+function displayQueue() {
+    const queueElement = document.getElementById('queue');
+    queueElement.innerHTML = '';
+    for (let i = 0; i < queue.length; i++) {
+        const song = queue[i];
+        const listItem = document.createElement('div');
+        listItem.textContent = `Song ${i + 1}: ${song.name}`;
+        queueElement.appendChild(listItem);
+    }
+}
+function displayQueue() {
+const queueElement = document.getElementById('queue');
+queueElement.innerHTML = '';
+for (let i = 0; i < queue.length; i++) {
+const song = queue[i];
+const listItem = document.createElement('div');
+listItem.textContent = `Song ${i + 1}: ${song.name}`;
+listItem.draggable = true; // Make the item draggable
+listItem.setAttribute('data-index', i); // Add data-index attribute to track the item's position
+listItem.setAttribute('ondragstart', 'drag(event)'); // Add dragstart event handler
+queueElement.appendChild(listItem);
+}
+}
+
+
+function allowDrop(event) {
+event.preventDefault();
+}
+
+function drag(event) {
+event.dataTransfer.setData('text', event.target.getAttribute('data-index'));
+}
+
+function drop(event) {
+event.preventDefault();
+const fromIndex = event.dataTransfer.getData('text');
+const toIndex = event.target.getAttribute('data-index');
+
+// Swap the songs in the queue array based on the dragged positions
+const temp = queue[fromIndex];
+queue[fromIndex] = queue[toIndex];
+queue[toIndex] = temp;
+
+// Update the displayed queue
+displayQueue();
+}
